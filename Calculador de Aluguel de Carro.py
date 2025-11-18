ad80c8405f1ad926f6755fca098d5f7e9db48fa0
Fernando Dias <fe.dias.brito28@gmail.com>
2025-11-17 22:28:01 -0300

    Delete Calculador de Aluguel de Carro.py

--- Calculador de Aluguel de Carro.py
+++
@@ -1,4 +0,0 @@
-dias = int(input('Digite a quantidade de dias que o carro foi alugado: '))
-km = float(input('Digite a quantidade de km que o carro percorreu: '))
-valor = (dias * 60) + (km * 0.15)
-print('O valor total a ser pago é de R$ {:.2f}'.format(valor))
